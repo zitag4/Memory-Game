717e83c038d05e3206fddf7f93b3bb1ad5d61f98
Zita Balint <zitag4@gmail.com>
2018-07-02 20:55:05 +0200

    Clean up the code

--- js/app.js
+++ js/app.js
@@ -7,27 +7,7 @@ let min = 0;
 let s = 0;
 let startedGame = false;
 
- // Create a list that holds all of your cards
-/*const cardList = [
-  "fa fa-diamond", "fa fa-diamond",
-  "fa fa-paper-plane-o", "fa fa-paper-plane-o",
-  "fa fa-anchor", "fa fa-anchor",
-  "fa fa-bolt", "fa fa-bolt",
-  "fa fa-cube", "fa fa-cube",
-  "fa fa-leaf", "fa fa-leaf",
-  "fa fa-bicycle" ,"fa fa-bicycle",
-  "fa fa-bomb", "fa fa-bomb"
-];*/
-
 let deck = document.querySelector('.deck');
-//Start the Game
-let cardList= document.getElementsByClassName('card');
-let cardArray=[];
-
-  for(let i=0; i<=15; i++) {
-    cardArray[i]=cardList.item(i);
-  }
-
 
 /*
  * Display the cards on the page
@@ -35,6 +15,14 @@ let cardArray=[];
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+//Get list elements from html
+let cardList= document.getElementsByClassName('card');
+let cardArray=[];
+
+for(let i=0; i<=15; i++) {
+  cardArray[i]=cardList.item(i);
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -51,8 +39,6 @@ function shuffle(array) {
     return array;
 }
 
-
-
 function showCard (card) {
   card.classList.add('open', 'show');
   clickCounter += 1;
@@ -62,30 +48,26 @@ function matchCard () {
   openCardsList[0].classList.add('match');
   openCardsList[1].classList.add('match');
   openCardsList = [];
-
 }
 
 function differentCard () {
   setTimeout(function() {
-  openCardsList[0].classList.remove('open', 'show');
-  openCardsList[1].classList.remove('open', 'show');
-  openCardsList = [];
-}, 800)
+    openCardsList[0].classList.remove('open', 'show');
+    openCardsList[1].classList.remove('open', 'show');
+    openCardsList = [];
+  }, 800)
 }
 
 // Win if all card are open
 function winGame (card) {
-  if (document.getElementsByClassName('match').length == 2){
+  if (document.getElementsByClassName('match').length == 16){
     document.querySelector('.win').style.cssText = 'z-index: 1;';
     document.querySelector('.score').innerHTML = 'whitin ' + min +':' + s + ' and with ' + star + ' <i class="fa fa-star"></i>';
     clearInterval(time);
   }
 }
 
-
-
 function timer() {
-
   time = setInterval( function () {
     s++;
     if (s == 60) {
@@ -93,28 +75,25 @@ function timer() {
       s = 0;
     }
     document.querySelector('.timer').textContent = 'Time: ' + min +':' + s;
-
   }, 1000)
 }
 
-function startGame () {
-
-}
-
+// Initialize the game
 function initGame() {
   startedGame = false;
-  document.querySelector('.win').style.cssText = 'z-index: -1;'
-  let shuffledCardList=shuffle(cardArray);
+  document.querySelector('.win').style.cssText = 'z-index: -1;';
 
+  //Add schuffled cards to HTML
+  let shuffledCardList=shuffle(cardArray);
   for(let i=0; i<=15; i++) {
     deck.appendChild(shuffledCardList[i]);
     shuffledCardList[i].classList.remove('open', 'show', 'match');
   }
 
-  clearInterval(time);
   moves = 0;
   document.querySelector('.moves').innerHTML = 0;
-  t = 0;
+
+  clearInterval(time);
   min = 0;
   s = 0;
   document.querySelector('.timer').innerHTML = 'Time: 00:00';
@@ -123,39 +102,44 @@ function initGame() {
 
 //Reload the game
 document.querySelector('.restart').addEventListener('click', initGame);
-
 document.querySelector('.again').addEventListener('click', initGame);
 
 deck.addEventListener('click', function (event) {
-
-  //Start timer
-  if(!startedGame){
-  timer();
-  startedGame = true;
-}
   let card = event.target;
-  //Add clicked cards to a list
-  if (!card.classList.contains('match')) {
-    if (openCardsList.length < 2){
-      showCard(card);
-      openCardsList.push(card);
+  //Drop every event which is not a card
+  if (event.target.classList.contains('card')) {
+    //Start timer
+    if(!startedGame){
+      timer();
+      startedGame = true;
     }
-    //Check cards
-    if(openCardsList.length > 1){
-      if(openCardsList[0].innerHTML === openCardsList[1].innerHTML){
-        matchCard();
-      }
-      else {
-        differentCard();
-      }
-      moves++;
-      document.querySelector('.moves').innerText = moves;
-      console.log(moves);
 
-      //Rating
-      if (moves/5 === 2 || moves/5 === 3 || moves/5 === 4) {
-        document.querySelector('.fa-star').remove();
-        star = 4-moves/5;
+    //Add clicked cards to a list
+    if (!card.classList.contains('match')) {
+      if (openCardsList.length < 2){
+        showCard(card);
+        openCardsList.push(card);
+      }
+
+      //Check clicked cards if match
+      if(openCardsList.length > 1){
+        if(openCardsList[0].innerHTML === openCardsList[1].innerHTML){
+          matchCard();
+        }
+        else {
+          differentCard();
+        }
+
+        //Incrementing moves
+        moves++;
+        document.querySelector('.moves').innerText = moves;
+        console.log(moves);
+
+        //Rating
+        if (moves/5 === 2 || moves/5 === 3 || moves/5 === 4) {
+          document.querySelector('.fa-star').remove();
+          star = 4-moves/5;
+        }
       }
     }
   }
@@ -163,22 +147,7 @@ deck.addEventListener('click', function (event) {
   winGame(card);
 });
 
-startGame();
-
-
-
-
-// Add open class when clicked
-/*document.addEventListener('click', function () {
-  const listElement = document.getElementsByClassName('card');
-  for(let i=0; i<listElement.length; i++) {
-    listElement[i].classList.add('open');
-    console.log(listElement[i]);
-  }
-});*/
-
-
-
+initGame();
 
 /*
  * set up the event listener for a card. If a card is clicked:
